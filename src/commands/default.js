543d4671dd65f2d349db68e52449a53d25559ba3
Pavol Otcenas <pavol@otcenas.eu>
2016-03-17 09:55:35 +0100

    Skip single error

--- src/commands/default.js
+++ src/commands/default.js
@@ -116,7 +116,7 @@ const handler = (payload, res) => {
     console.log('Getting menus for [' + list.length + '] Restaurants');
 
     async.parallel(getMenuCallbacks, function(err, attachments){
-      //if (err) throw err
+      if (err) return;
 
       console.log('Finished. Menus:', attachments);
 
